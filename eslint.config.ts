998eb484091470242fef1b3cb1a4d5c7717686c9
dnmrcprds <dnmrcprds@gmail.com>
2026-08-08 18:36:55 +0800

    feat: initial setup of prettier and eslint

---
+++ eslint.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig, globalIgnores } from "eslint/config"
+import nextVitals from "eslint-config-next/core-web-vitals"
+import nextTs from "eslint-config-next/typescript"
+import tseslint from "typescript-eslint"
+
+module.exports = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  ...tseslint.configs.recommended,
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+  {
+    rules: {
+      "@typescript-eslint/consistent-type-imports": [
+        2,
+        {
+          prefer: "type-imports",
+          fixStyle: "separate-type-imports",
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        2,
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/no-explicit-any": 2,
+      "no-console": 1,
+      "no-duplicate-imports": "error",
+    },
+  },
+])
